02ff523ac433589a7ee8e9dddac97eae42ed97de
phoenix0x1 <phoenix0x.web3@gmail.com>
2025-10-09 22:23:47 +0400

    Add banned account handling to the menu
    Co-authored-by: phoenix0x1 <221748789+phoenix0x1@users.noreply.github.com>

--- runtimes/linux_py31211/pyarmor_runtime_000000/__init__.py
+++ runtimes/linux_py31211/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-10-06T20:14:12.321582
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-09T18:21:43.318313
 from .pyarmor_runtime import __pyarmor__
--- runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-10-06T20:14:08.666761
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-09T18:21:45.345820
 from .pyarmor_runtime import __pyarmor__
