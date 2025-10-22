926515d45ef336e6aa05704743232cd0ed5066cf
phoenix0x1 <phoenix0x.web3@gmail.com>
2025-10-22 14:23:09 +0400

    fix for claiming rewards
    Co-authored-by: phoenix0x1 <221748789+phoenix0x1@users.noreply.github.com>

--- runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-10-19T20:48:57.203059
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-22T10:21:33.699416
 from .pyarmor_runtime import __pyarmor__
--- runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (trial), 000000, 2025-10-19T20:49:23.874123
+# Pyarmor 9.1.9 (trial), 000000, 2025-10-22T10:21:51.156902
 from .pyarmor_runtime import __pyarmor__
